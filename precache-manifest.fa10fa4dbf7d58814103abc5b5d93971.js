1a76576dd7811a06e9668167cde8ea5275c8b722
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-10-28 21:30:15 +0200

    Updates

---
+++ precache-manifest.fa10fa4dbf7d58814103abc5b5d93971.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9428b3510c2db9df77fab0ae6aef96c",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "ffcd2fc25e03de24b18a",
+    "url": "/goit-react-hw-01-components/static/css/main.f80212e6.chunk.css"
+  },
+  {
+    "revision": "9d33ab986474be4d9ab6",
+    "url": "/goit-react-hw-01-components/static/js/2.7c8250cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.7c8250cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffcd2fc25e03de24b18a",
+    "url": "/goit-react-hw-01-components/static/js/main.abc424ac.chunk.js"
+  },
+  {
+    "revision": "a215c175bf92be1f6222",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.3c81574f.js"
+  }
+]);
